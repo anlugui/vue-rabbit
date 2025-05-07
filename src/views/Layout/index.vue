6fcd8c7ca71cc84c152beb7787a8681183444f21
anlugui <1767284486@qq.com>
2025-05-07 18:58:14 +0800

    layout-静态结构搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,15 +1,16 @@
 <template>
-    <div>
-      我是首页
+ 
+      <LayoutNav/>
+      <LayoutHeader/>
       <!-- 二级路由出口 -->
        <router-view></router-view>
-    </div>
+      <LayoutFooter/>
   </template>
   
-  <script>
-  export default {
-  
-  }
+  <script setup>
+  import LayoutNav from './components/LayoutNav.vue';
+  import LayoutHeader from './components/LayoutHeader.vue';
+  import LayoutFooter from './components/LayoutFooter.vue';
   </script>
   
   <style>
