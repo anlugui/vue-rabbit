856d074b3346aa7cd6e2103a47000993aa4b59a3
anlugui <1767284486@qq.com>
2025-05-07 17:30:17 +0800

    路由配置

---
+++ src/views/Layout/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      我是首页
+      <!-- 二级路由出口 -->
+       <router-view></router-view>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  <style>
+  
+  </style>
