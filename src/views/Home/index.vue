230ecf5b2eb1cf4278c69d20ed62e83a55af91a7
anlugui <1767284486@qq.com>
2025-05-24 19:33:27 +0800

    home-面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,7 +3,8 @@ import HomeCategory from './Components/HomeCategory.vue'
 import HomeBanner from './Components/HomeBanner.vue'
 import HomeNew from './Components/HomeNew.vue'
 import HomeHot from './Components/HomeHot.vue'
-import homeProduct from './Components/HomeProduct.vue'
+import HomeProduct from './Components/HomeProduct.vue'
+import HomePanel from './Components/HomePanel.vue'
 </script>
 
 <template>
@@ -13,5 +14,12 @@ import homeProduct from './Components/HomeProduct.vue'
   </div>
   <HomeNew />
   <HomeHot />
-  <homeProduct />
+  <HomeProduct />
+  <!-- 测试面板组件 -->
+    <HomePanel  title="新鲜好物" sub-title="新鲜好物 好多商品">
+    <div>我是新鲜好物的插槽</div>  
+    </HomePanel>
+    <HomePanel  title="人气推荐" sub-title="人气推荐 好多商品"> 
+     <div> 我是人气推荐的插槽内容</div>
+    </HomePanel>
 </template>
